2aeb03c764fdce5b4dcf6873f7f1b76662ff3685
Sean Johnsen <sean.b.johnsen@gmail.com>
2021-06-16 13:58:31 -0400

    add doom config

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/gdrive/slipbox/20210601111140-steffen2014_what_are_memories_made_of_how_polycomb_and_trithorax_proteins_mediate_epigenetic_memory.org" "~/Documents/mckay_lab_notebook/Projects/ATAC_Discs/developmental_staging_notebook.org" "/Users/seanjohnsen/Documents/mckay_lab_notebook/ATAC-Seq on Developing Imaginal Disc.org" "/Users/seanjohnsen/Documents/mckay_lab_notebook/README.org" "/Users/seanjohnsen/Documents/mckay_lab_notebook/safety.org" "/Users/seanjohnsen/Documents/mckay_lab_notebook/strains.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
